d8163f5bccc29c09ca149261963dc68a7f2dd9c0
Tashkinov Viktor <114143145+MrRaver@users.noreply.github.com>
2023-04-10 23:23:46 +0500

    Update users_page.py

--- pages/users_page.py
+++ pages/users_page.py
@@ -90,6 +90,31 @@ class User(BasePage):
         alert.accept()
         return self.element_is_visible(user.TEXT_ERROR).text
 
+    def GR12_password_test(self):  #7.Пользователи (Несовпадающие пароли)
+        self.autorization()
+        self.element_is_visible(user.USERS).click()
+        self.element_is_visible(user.ADD_USERS).click()
+        self.element_is_visible(user.LOGIN_USER).send_keys("Тестовая роль GR12")
+        self.element_is_visible(user.EMAIL_USER).send_keys("testgr@test.ru")
+        self.element_is_visible(user.LASTNAME_USER).send_keys("test")
+        self.element_is_visible(user.FIRSTNAME_USER).send_keys("Тестовая роль GR12")
+        self.element_is_visible(user.SURNAME_USER).send_keys("test")
+        self.element_is_visible(user.PASSWORD_USER).send_keys("12345")
+        self.element_is_visible(user.PASSWORD_USER2).send_keys("123456")
+        return self.element_is_visible(user.PASSWORD_NOT_MATHES).text
+    def GR12_name_test(self):  #8.Пользователи (Отсутствует Имя пользователя)
+        self.autorization()
+        self.element_is_visible(user.USERS).click()
+        self.element_is_visible(user.ADD_USERS).click()
+        self.element_is_visible(user.LOGIN_USER).send_keys("Тестовая роль GR12")
+        self.element_is_visible(user.EMAIL_USER).send_keys("testgr@test.ru")
+        self.element_is_visible(user.LASTNAME_USER).send_keys("test")
+        self.element_is_visible(user.SURNAME_USER).send_keys("test")
+        self.element_is_visible(user.PASSWORD_USER).send_keys("12345")
+        self.element_is_visible(user.PASSWORD_USER2).send_keys("12345")
+        self.element_is_visible(user.SAVE_USERS).click()
+        return self.element_is_visible(user.NOT_NAMES).text
+
 
     def add_role_GR1(self): #11 Добавление роли в таблицу
         self.autorization()
@@ -120,5 +145,56 @@ class User(BasePage):
         self.element_is_visible(user.DELETE_GR1).click()
         self.element_is_visible(user.OKAY).click()
         self.element_is_visible(user.SAVE).click()
+
+    def tree_status(self):  # 13.Дерево статусов
+        self.autorization()
+        self.element_is_visible(user.EDIT_ADDITIONAL).click()
+        self.element_is_visible(user.THE_RIGHT).click()
+        self.element_is_visible(user.BUSINESS_PROCESS).click()
+        self.element_is_present(user.TRANSLITION_ALL_STATUS).click()
+        self.element_is_present(user.SETTING_STATUS).click()
+        self.element_is_present(user.CHER_READY).click()
+        self.element_is_visible(user.SAVE_BUSINESS_STATUS).click()
+        self.element_is_visible(user.SAVE).click()
+        self.element_is_visible(user.TREE_STATUS).click()
+
+    def role2_inheritance(self):  # 14.Наследование
+        self.autorization()
+        self.element_is_visible(user.EDIT_ORDER).click()
+        self.element_is_visible(user.THE_RIGHT).click()
+        self.element_is_visible(user.ADD_THE_RIGHT).click()
+        self.element_is_visible(user.NAMES_ROLE).click()
+        self.element_is_visible(user.NAMES_ROLE_ENTER).click()
+        names = self.element_is_visible(user.INPUT_NAMES_ROLE_ENTER)
+        names.send_keys("GR1")
+        names.send_keys(Keys.ENTER)
+        self.element_is_visible(user.SEES).click()
+        self.element_is_visible(user.FIND_ELEMENT).click()
+        self.element_is_visible(user.NUMBER_ID).click()
+        self.element_is_visible(user.STRING).click()
+        self.element_is_visible(user.OK).click()
+        self.element_is_present(user.CHECKBOX_ALL_STATUS).click()
+        self.element_is_visible(user.EDITS).click()
+        self.element_is_present(user.ADD_FIELD).click()
+        self.element_is_present(user.EDIT_FIELD).click()
+        self.element_is_visible(user.BUSINESS_PROCESS).click()
+        self.element_is_present(user.TRANSLITION_ALL_STATUS).click()
+        self.element_is_visible(user.SAVE).click()
+        self.element_is_visible(user.ROLES).click()
+        self.element_is_visible(user.TEST_ROLE_GR2).click()
+        self.element_is_visible(user.EYES).click()
+        self.element_is_visible(user.CHOICE_EL_GR2).click()
+        input = self.element_is_visible(user.PLACEHOLDER_GR2)
+        input.send_keys("Тестовая роль GR2")
+        input.send_keys(Keys.ENTER)
+        self.element_is_visible(user.SAVE_GR2).click()
+        self.element_is_visible(user.ROLES).click()
+        self.element_is_visible(user.TEST_ROLE_GR2).click()
+        self.element_is_visible(user.TEST_SCHEMA).click()
+
+
+
+
+
 
 
